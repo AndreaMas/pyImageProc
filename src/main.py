d38e226ffefa409d77e32e24f21fe6f83424d8e4
Andrea <andrea.masciulli@me.com>
2024-10-06 23:37:51 +0200

    some comments added

--- src/main.py
+++ src/main.py
@@ -2,9 +2,13 @@ import sys
 from application import Application
 
 if __name__ == "__main__":
-    # Create Application (inherits from QApplication)
-    # Application contains MainWindow (inherits from QMainWindow)
+
+    # Application inherits from QApplication
+    # App contains MainWindow (which inherits from QMainWindow), the UI side of the app.
+    # App also contains ImageProcessor, the Core side of the app.
+    # App works as a listener to MainWindow and ImageProcessor, hearing their signals, enabling communication betw UI and Core.
+
     app = Application(sys.argv)
-    sys.exit(app.exec()) # execute's QApplication's main loop, which lets signals work
+    sys.exit(app.exec()) # exec() executes QApplication's main loop, which lets signals work
 
     
